d8d2faf76d27059039426998c21b0174bb29a354
JY YOON <139187596+duckrom@users.noreply.github.com>
2023-08-30 20:41:54 -0700

    Add files via upload

---
+++ 2023.08.28/test1.js
@@ -0,0 +1,18 @@
+var Objct1 = {
+    email:"abc@gmail.com",
+    name:"abc"
+}
+
+var Objct2 = {
+    email:"xyz@gmail.com",
+    name:"xyz"
+}
+
+class User{
+    constructor(email, name){
+        email:email;
+        name:name;
+    }//initialize the object
+}
+
+var a = new User('abc@gmail.com', 'abc')
